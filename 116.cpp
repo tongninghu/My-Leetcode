e289d087b9524db487d32e095654239c5c6293bb
tongninghu <tongning.hu@gmail.com>
2017-04-17 21:38:57 -0700

    Create 116.cpp

---
+++ 116.cpp
@@ -0,0 +1,52 @@
+Given a binary tree
+
+    struct TreeLinkNode {
+      TreeLinkNode *left;
+      TreeLinkNode *right;
+      TreeLinkNode *next;
+    }
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+Note:
+
+You may only use constant extra space.
+You may assume that it is a perfect binary tree (ie, all leaves are at the same level, and every parent has two children).
+For example,
+Given the following perfect binary tree,
+         1
+       /  \
+      2    3
+     / \  / \
+    4  5  6  7
+After calling your function, the tree should look like:
+         1 -> NULL
+       /  \
+      2 -> 3 -> NULL
+     / \  / \
+    4->5->6->7 -> NULL
+
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(TreeLinkNode *left, TreeLinkNode *right) {
+        if (left) helper(left->left, left->right);
+        if (left && right) {
+            helper(left->right, right->left);
+            left->next = right;
+            right->next = NULL;
+        }
+        if (right) helper(right->left, right->right);
+    }
+    void connect(TreeLinkNode *root) {
+        if (root)  helper(root->left, root->right);
+    }
+};
