8d654ae61b2ab60a87b549b9859581d315f15fa5
tongninghu <tongning.hu@gmail.com>
2017-04-23 13:57:26 -0700

    Update and rename 153 to 153.cpp

---
+++ 153.cpp
@@ -0,0 +1,20 @@
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+Find the minimum element.
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        int low = 0, high = nums.size() - 1, mid;
+        while (low < high) {
+            mid = (high - low) / 2 + low;
+            if (nums[mid] < nums[low]) high = mid;
+            else if (nums[mid] > nums[high]) low = mid + 1;
+            else return nums[low];
+        }
+        return nums[low];
+    }
+};
