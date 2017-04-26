79f1095dc313764937f7b9af86e726ca9bed134c
tongninghu <tongning.hu@gmail.com>
2017-04-26 16:58:28 -0700

    Update 207.cpp

--- 207.cpp
+++ 207.cpp
@@ -25,7 +25,7 @@ public:
         vector<int> visited (numCourses, 0);
         bool flag = true;
         for(int i = 0; i < graph.size(); i++) {
-             helper(graph, visited, i, flag); 
+             if (visited[i] == 0) helper(graph, visited, i, flag); 
         }
         return flag;
     }
