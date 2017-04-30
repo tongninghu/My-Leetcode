84b6fb7aa3c33007fd24adee9944b8130cfe71aa
tongninghu <tongning.hu@gmail.com>
2017-04-29 22:56:56 -0700

    Create 382.cpp

---
+++ 382.cpp
@@ -0,0 +1,56 @@
+Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+
+Follow up:
+What if the linked list is extremely large and its length is unknown to you? Could you solve this efficiently without using extra space?
+
+Example:
+
+// Init a singly linked list [1,2,3].
+ListNode head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+Solution solution = new Solution(head);
+
+// getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+solution.getRandom();
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode* Head;
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    Solution(ListNode* head) {
+        this->Head = head;
+    }
+    
+    /** Returns a random node's value. */
+    int getRandom() {
+        int n = 0, res;
+        for(ListNode* ptr = Head; ptr; ptr = ptr->next) {
+            if (n == 0) {
+                res = ptr->val;
+                n++;
+            }
+            else {
+                n++;
+                if (rand() % n == 0) res = ptr->val;
+            }
+        }
+        return res;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(head);
+ * int param_1 = obj.getRandom();
+ */
