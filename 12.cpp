fc968bccc3aa90c422d88472970ff3ffbbe6d0fd
tongninghu <tongning.hu@gmail.com>
2017-05-02 01:46:21 -0700

    Update 12.cpp

--- 12.cpp
+++ 12.cpp
@@ -8,18 +8,11 @@ public:
         string s;
         vector<int> my_int {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
         unordered_map<int, string> my_map {{1, "I"}, {4, "IV"}, {5, "V"}, {9, "IX"}, {10, "X"}, {40, "XL"}, {50, "L"}, {90, "XC"}, {100, "C"}, {400, "CD"},{500, "D"}, {900, "CM"}, {1000, "M"}};
-        int c, d = 10;
-        while (num > 0) {
-            c = num % d;
-            num -= c;
-            int i = upper_bound(my_int.begin(), my_int.end(), c) - my_int.begin();
-            string s2 = my_map[my_int[i - 1]];
-            while ((c - my_int[i - 1]) > 0) {
-                s2 = s2 + my_map[d / 10];
-                c = c - d / 10;
+        for (int i = my_int.size() - 1; i >= 0; i--) {
+            for (int n = 0; n < num / my_int[i]; n++) {
+                s += my_map[my_int[i]];
             }
-            d = d * 10;
-            s = s2 + s;
+            num = num - num / my_int[i] * my_int[i];
         }
         return s;
     }
