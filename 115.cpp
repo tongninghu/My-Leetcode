dccd477a0dc7eed625b48d021e9425a35ee7ad6e
tongninghu <tongning.hu@gmail.com>
2017-05-18 22:36:21 -0700

    Update 115.cpp

--- 115.cpp
+++ 115.cpp
@@ -12,14 +12,14 @@ public:
     int numDistinct(string s, string t) {
         int m = t.size(), n = s.size();
         vector<vector<int>> dp (m + 1, vector<int> (n + 1, 0));
+        dp[0][0] = 1;
         for (int j = 0; j < n; j++) 
             dp[0][j + 1] = 1;
         
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (t[i] != s[j]) dp[i + 1][j + 1] = dp[i + 1][j];
-                else if (i > 0 && t[i] == t[i - 1])  dp[i + 1][j + 1] = dp[i][j] + dp[i + 1][j];
-                else  dp[i + 1][j + 1] = dp[i][j + 1] + dp[i + 1][j];
+                else  dp[i + 1][j + 1] = dp[i][j] + dp[i + 1][j];
             }
         }
         return dp[m][n];
