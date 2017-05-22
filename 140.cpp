9c4445cf65cf46c29624496e26b4e97ac17ae0d3
tongninghu <tongning.hu@gmail.com>
2017-05-21 17:56:24 -0700

    Update 140.cpp

--- 140.cpp
+++ 140.cpp
@@ -9,49 +9,34 @@ dict = ["cat", "cats", "and", "sand", "dog"].
 A solution is ["cats and dog", "cat sand dog"].
 
 class Solution {
+    unordered_map<string, vector<string>> dp;
 public:
-    void dfs(int start, vector<vector<int>>& dp, vector<int>& cur, vector<string>& res, string &s) {
-        if(start == 0) {
-            int begin = 0;
-            string ss;
-            for(int k = cur.size() - 2; k >= 0; k--) {
-                ss += s.substr(begin, cur[k] - begin) + " ";
-                begin = cur[k];
-            }
-            ss += s.substr(cur[0]);
-            res.push_back(ss);
-            return;
-        }
-        for (int k = dp[start].size() - 1; k >= 0; k--) {
-            int i = dp[start][k];
-            cur.push_back(i);
-            dfs(i, dp, cur, res, s);
-            cur.pop_back();
-        }
-    }
-    vector<string> wordBreak(string s, vector<string>& wordDict) {
-        int m = s.size();
-        vector<int> cur;
+    vector<string> combine(string w, vector<string> t) {
         vector<string> res;
-        unordered_set<string> wordlist;
-        for (int i = 0; i < wordDict.size(); i++) 
-            wordlist.insert(wordDict[i]);
-        vector<vector<int>> dp(m + 1, vector<int>());
-        dp[0].push_back(0);
-        for (int i = 1; i <= m; i++) {
-            for (int j = i - 1; j >= 0; j--) {
-                if (!dp[j].empty()) {
-                    string word = s.substr(j,i-j);
-                    if (wordlist.find(word)!= wordlist.end())  dp[i].push_back(j);
-                }
-            }
-        }
-        for (int k = dp[m].size() - 1; k >= 0; k--) {
-            int i = dp[m][k];
-            cur.push_back(i);
-            dfs(i, dp, cur, res, s);
-            cur.pop_back();
+        for(int i = 0; i < t.size(); i++) {
+            res.push_back(w + " " + t[i]);
         }
         return res;
+    }
+    
+    vector<string> dfs(string s, unordered_set<string>& word) {
+        if (dp.count(s)) return dp[s];
+        vector<string> result;
+        if (word.find(s) != word.end()) result.push_back(s);
+        for(int i = 1; i < s.size(); i++) {
+            if (word.find(s.substr(0, i)) != word.end()) {
+                vector<string> cur = combine(s.substr(0, i), dfs(s.substr(i), word));
+                result.insert(result.end(), cur.begin(), cur.end());
+            }
+        }    
+        dp[s] = result;
+        return result;
+    }
+    
+    vector<string> wordBreak(string s, vector<string>& word) {
+        unordered_set<string> my_set;
+        for(int i = 0; i < word.size(); i++)
+            my_set.insert(word[i]);
+        return dfs (s, my_set);
     }
 };
