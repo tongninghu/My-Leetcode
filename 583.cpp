a1122aecf3030e82a9df17a617ea7757eb51a302
tongninghu <tongning.hu@gmail.com>
2017-07-17 22:47:11 -0700

    Create 583.cpp

---
+++ 583.cpp
@@ -0,0 +1,20 @@
+Given two words word1 and word2, find the minimum number of steps required to make word1 and word2 the same, where in each step you can delete one character in either string.
+
+Example 1:
+Input: "sea", "eat"
+Output: 2
+Explanation: You need one step to make "sea" to "ea" and another step to make "eat" to "ea".
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n1 = word1.size(), n2 = word2.size();
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+        for (int i = 0; i < n1; i++) 
+            for (int j = 0; j < n2; j++)
+                if (word1[i] == word2[j]) dp[i + 1][j + 1] = dp[i][j] + 1;
+                else dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j]);
+        
+        return (n1 + n2 - 2 * dp[n1][n2]);        
+    }
+};
