8b41e438ae7a8e2d4ee27cd22c162bcc4dfad808
tongninghu <tongning.hu@gmail.com>
2017-07-17 22:24:39 -0700

    Create 565.cpp

---
+++ 565.cpp
@@ -0,0 +1,43 @@
+A zero-indexed array A consisting of N different integers is given. The array contains all integers in the range [0, N - 1].
+
+Sets S[K] for 0 <= K < N are defined as follows:
+
+S[K] = { A[K], A[A[K]], A[A[A[K]]], ... }.
+
+Sets S[K] are finite for each K and should NOT contain duplicates.
+
+Write a function that given an array A consisting of N integers, return the size of the largest set S[K] for this array.
+
+Example 1:
+Input: A = [5,4,0,3,1,6,2]
+Output: 4
+Explanation: 
+A[0] = 5, A[1] = 4, A[2] = 0, A[3] = 3, A[4] = 1, A[5] = 6, A[6] = 2.
+
+One of the longest S[K]:
+S[0] = {A[0], A[5], A[6], A[2]} = {5, 6, 2, 0}
+
+class Solution {
+public:
+    int arrayNesting(vector<int>& nums) {
+        unordered_set<int> NUM, temp;
+        int t = 0;
+        for (int i = 0; i < nums.size(); i++) 
+            NUM.insert(i);
+        
+        while (NUM.size() > t) {
+            int j = *NUM.begin();
+            NUM.erase(j);
+            temp.insert(j);
+            j = nums[j];
+            while (temp.find(j) == temp.end()) {
+                NUM.erase(j);
+                temp.insert(j);
+                j = nums[j];
+            }
+            t = max(t, (int) temp.size());
+            temp.clear();
+        }
+        return t;
+    }
+};
