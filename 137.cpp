2fb30baae67e5bb0b660cd5e64eb294f7c86a447
tongninghu <tongning.hu@gmail.com>
2017-08-15 23:53:34 -0700

    Create 137.cpp

---
+++ 137.cpp
@@ -0,0 +1,23 @@
+Given an array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        //#curent  income  ouput
+        //# ab      c/c       ab/ab
+        //# 00      1/0       01/00
+        //# 01      1/0       10/01
+        //# 10      1/0       00/10
+        // a=~abc+a~b~c;
+        // b=~a~bc+~ab~c;
+        int a=0;
+        int b=0;
+        for(int c:nums){
+            int ta=(~a&b&c)|(a&~b&~c);
+            b=(~a&~b&c)|(~a&b&~c);
+            a=ta;
+        }
+        //we need find the number that is 01,10 => 1, 00 => 0.
+        return a|b;
+    }
+};
