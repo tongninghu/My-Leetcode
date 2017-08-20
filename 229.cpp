150b3c076091394882625474329c1b8d17d31575
tongninghu <tongning.hu@gmail.com>
2017-08-19 18:24:14 -0700

    Create 229.cpp

---
+++ 229.cpp
@@ -0,0 +1,40 @@
+Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times. The algorithm should run in linear time and in O(1) space.
+
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int cnt1 = 0, cnt2 = 0, a = 0, b = 0;
+    
+        for(int n: nums){
+            if (a == n){
+                cnt1++;
+            }
+            else if (b == n){
+                cnt2++;
+            }
+            else if (cnt1 == 0){
+                a = n;
+                cnt1 = 1;
+            }
+            else if (cnt2 == 0){
+                b = n;
+                cnt2 = 1;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
+        }
+    
+        cnt1 = cnt2 = 0;
+        for(int n: nums){
+            if (n == a)   cnt1++;
+            else if (n == b)  cnt2++;
+        }
+    
+        vector<int> res;
+        if (cnt1 > nums.size()/3)   res.push_back(a);
+        if (cnt2 > nums.size()/3)   res.push_back(b);
+        return res;      
+    }
+};
