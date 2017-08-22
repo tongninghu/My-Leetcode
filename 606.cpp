2867b544a8f29c90ecbac59e498dc4b15d3a3ff9
tongninghu <tongning.hu@gmail.com>
2017-08-22 12:43:33 -0700

    Create 606.cpp

---
+++ 606.cpp
@@ -0,0 +1,64 @@
+You need to construct a string consists of parenthesis and integers from a binary tree with the preorder traversing way.
+
+The null node needs to be represented by empty parenthesis pair "()". And you need to omit all the empty parenthesis pairs that don't affect the one-to-one mapping relationship between the string and the original binary tree.
+
+Example 1:
+Input: Binary tree: [1,2,3,4]
+       1
+     /   \
+    2     3
+   /    
+  4     
+
+Output: "1(2(4))(3)"
+
+Explanation: Originallay it needs to be "1(2(4)())(3()())", 
+but you need to omit all the unnecessary empty parenthesis pairs. 
+And it will be "1(2(4))(3)".
+Example 2:
+Input: Binary tree: [1,2,3,null,4]
+       1
+     /   \
+    2     3
+     \  
+      4 
+
+Output: "1(2()(4))(3)"
+
+Explanation: Almost the same as the first example, 
+except we can't omit the first parenthesis pair to break the one-to-one mapping relationship between the input an
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    string construct(TreeNode* t) {
+        string res = "(";
+        if (t) {
+            res += to_string(t->val);
+            string l = construct(t->left);
+            string r = construct(t->right);
+            if (r != "()") {
+                res += l;
+                res += r;
+            }
+            else if (l != "()" && r == "()") {
+                res += l;
+            }
+        }
+        res += ')';
+        return res;
+    }
+    
+    string tree2str(TreeNode* t) {
+        string res = construct(t);
+        return res.substr(1, res.length() - 2);
+    }
+};
