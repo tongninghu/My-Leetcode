39fabcc99a55c84c1334faf82a10a432b3f0134f
tongninghu <tongning.hu@gmail.com>
2017-08-22 16:30:40 -0700

    Update 658.cpp

--- 658.cpp
+++ 658.cpp
@@ -10,22 +10,24 @@ Output: [1,2,3,4]
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        vector<int> res;
+        vector<int> res, small, large;
         int i = upper_bound(arr.begin(), arr.end(), x) - arr.begin();
         if (arr[i] != x) {
             i--;
         }
         int j = i + 1;
         
-        while (res.size() < k) {
+        while (small.size() + large.size() < k) {
             if (i >= 0 && j < arr.size() && abs(arr[i] - x) <= abs(arr[j] - x) || j == arr.size()) {
-                res.push_back(arr[i--]);
+                small.push_back(arr[i--]);
             }
             else if (i >= 0 && j < arr.size() && abs(arr[i] - x) > abs(arr[j] - x) || i < 0) {
-                res.push_back(arr[j++]);
+                large.push_back(arr[j++]);
             }
         }
-        sort(res.begin(), res.end());
+        reverse(small.begin(), small.end());
+        res = small;
+        res.insert(res.end(), large.begin(), large.end());
         return res;
     }
 };
